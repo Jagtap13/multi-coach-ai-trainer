50abb9950e3f4e3a57c490a6fdf6027d6c96c48f
Jagtap13 <jagtapadi13@gmail.com>
2026-08-04 17:18:32 +0530

    Fix Neon Connection with drops with pool pre ping and pool recycle

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -10,7 +10,11 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL not found in environment. Check your .env file.")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300,
+    )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
